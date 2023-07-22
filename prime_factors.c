9f69575ae98dec92091e28db7df36ae47cb9d320
Ivan Ashikhmin <ashikhmin.1103@gmail.com>
2023-07-22 11:49:15 +0300

    prime_factors

---
+++ prime_factors.c
@@ -0,0 +1,19 @@
+#include <math.h>
+
+int prime_factors (unsigned long long n, unsigned long long factors[])
+{ int count=0; 
+   for ( int j = 2; j<=sqrt(n); j++) 
+   {
+       while (n%j==0)
+             {
+                factors[count++] = j; 
+                n=n/j;
+             }
+     
+    } 
+  if(n!=1) 
+  {
+    factors[count++] = n;
+  }
+  return count;
+}
