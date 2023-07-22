402479bfb6c2e3b73b6f9ce6ae5c99f352a4e19c
Ivan Ashikhmin <ashikhmin.1103@gmail.com>
2023-07-22 11:47:27 +0300

    permute_a_palindrome

---
+++ permute_a_palindrome.c
@@ -0,0 +1,29 @@
+#include <stdbool.h>
+bool permute_a_palindrome(const char *str_in) 
+{
+int count_symbols[26] = {0};
+  int symbols = 0;
+  int count = 0;
+  int res = 0;
+  
+while(*(str_in+symbols)){symbols++;};
+int mas2[symbols];
+  
+ for(int i = 0; i < symbols; i++)
+   {
+   count = str_in[i] - 'a'; 
+   count_symbols[count]++;  
+ }
+  
+  for(int i = 0; i < 26; i++ ) 
+  {
+    if (count_symbols[i] % 2 != 0)
+      {
+      res++;
+      if(res > 1){
+        {return false;}
+                    }
+    }
+  } 
+  return true;
+}
