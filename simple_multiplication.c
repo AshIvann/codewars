fe1faee81aa17183f07454d57291b5b59d98749c
AshIvann <127615148+AshIvann@users.noreply.github.com>
2023-09-07 14:31:37 +0300

    Update simple_multiplication.c

--- simple_multiplication.c
+++ simple_multiplication.c
@@ -1,12 +1,6 @@
 //https://www.codewars.com/kata/583710ccaa6717322c000105
-int simple_multiplication(int number) {
-    // your code
-  if(number % 2 == 0)
-    {
-      return number * 8;    
-    }
-  else
-    {
-      return number * 9;    
-    }
+
+int simple_multiplication(int number) 
+{
+    return number = (number % 2 == 0) ? (number << 3) : (number << 3) + number ;
 }
